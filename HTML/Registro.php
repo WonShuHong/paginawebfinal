70817ced1108cdc21ada771e40c9f0959010ac07
WonShuHong <whon074@g.educaand.es>
2024-05-07 10:25:51 +0200

    casa 2

---
+++ HTML/Registro.php
@@ -0,0 +1,29 @@
+<?php
+   
+   $nombreServidor = "localhost";
+   $baseDatos = "Ejemplo";
+   $nombreUsuario = "paginawebfinal";
+   $password = "Usuario";
+   
+   // Se crea la conexión
+   
+   $conn = new mysqli($nombreServidor, $nombreUsuario, $password, $baseDatos);
+   
+   // Se comprueba que la conexión es correcta
+   
+    if (!$conn) {
+       die("Connection failed: " . mysqli_connect_error());
+    }
+  echo "Conexión correcta";
+
+ // $sentenciaSQL="INSERT INTO Localidad VALUES(3,'Córdoba')";
+
+    $sentenciaSQL="INSERT INTO Entregas VALUES(null,2024-03-12 19:30,234234234,'45kg','Arbol',4,4)";
+    if (!$conn->query($sentenciaSQL)) {
+      
+      echo "Falló la inserción de datos en la tabla: (" . $conn->errno . ") " . $conn->error;
+    
+    }
+    echo $sentenciaSQL;
+    mysqli_close($conn);
+?>
